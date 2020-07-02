b25cb840a60dfe2628a036b4fb41355631fbe7e0
Jamie Gregory <jamiegregsa@gmail.com>
2020-07-02 18:18:23 +0200

    wip storybook

---
+++ plugin-test/stories/0-Welcome.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { List } from "@dadoagency/gatsby-plugin-trustpilot-widget"//"../../gatsby-plugin-trustpilot-widget/src/List"
+
+export default {
+  title: 'List',
+  component: List,
+};
+
+export const Default = () => <List />;
+
+// function List() {
+//   return (
+
+//     <div>Im a list</div>
+//   )
+// }
