ac09781f5b6d3302fd3308d332059b4e1e780c13
Martin Kruger <martin@martinkruger.com>
2021-01-21 14:13:28 +0200

    Added Horizontal template

---
+++ src/Horizontal.js
@@ -0,0 +1,47 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import templates from './templates';
+import TrustpilotContainer from './TrustpilotContainer';
+import useSitePluginOptions from './hooks/useSitePluginOptions';
+
+const Horizontal = ({ language, culture, theme, height, width, stars }) => {
+  const reference = React.createRef();
+  const { business, username } = useSitePluginOptions();
+
+  return (
+    <Fragment>
+      <TrustpilotContainer
+        reference={reference}
+        template={templates.Horizontal}
+        business={business}
+        username={username}
+        language={language}
+        culture={culture}
+        theme={theme}
+        height={height}
+        width={width}
+        stars={stars}
+      />
+    </Fragment>
+  );
+};
+
+Horizontal.propTypes = {
+  language: PropTypes.string,
+  culture: PropTypes.string,
+  theme: PropTypes.string,
+  height: PropTypes.string,
+  width: PropTypes.string,
+  stars: PropTypes.number,
+};
+
+Horizontal.defaultProps = {
+  language: 'en',
+  culture: 'US',
+  theme: 'light',
+  height: '52px',
+  width: '100%',
+  stars: 5,
+};
+
+export default Horizontal;
