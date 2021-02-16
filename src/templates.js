ce316436f00c26a26aadb8fa0ca8d8bba2758311
Martin Kruger <martin@martinkruger.com>
2021-02-16 12:00:45 +0200

    Addeed MiniCarousel

--- src/templates.js
+++ src/templates.js
@@ -2,6 +2,7 @@ export default {
   Carousel: '53aa8912dec7e10d38f59f36',
   Horizontal: '5406e65db0d04a09e042d5fc',
   List: '539ad60defb9600b94d7df2c',
+  MiniCarousel: '539ad0ffdec7e10e686debd7',
   MicroStar: '5419b732fbfb950b10de65e5',
   MicroCombo: '5419b6ffb0d04a076446a9af',
 };
